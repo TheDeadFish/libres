c1d0adaca4353dd833ed79fe460b2f70bc4fa444
TheDeadFish <michaelstamper1@gmail.com>
2020-04-05 15:32:15 +0000

    moved files to src and src/app directies

--- Make.bat
+++ Make.bat
@@ -4,5 +4,5 @@
 @copy /Y bin\libres.exe %PROGRAMS%\bin
 @copy /Y bin\libmerge.exe %PROGRAMS%\bin
 @libmerge %PROGRAMS%\local\lib32\libexshit.a  bin\libres.a
-@copy /Y arFile.h %PROGRAMS%\local\include 
-@copy /Y object.h %PROGRAMS%\local\include 
+@copy /Y src\arFile.h %PROGRAMS%\local\include 
+@copy /Y src\object.h %PROGRAMS%\local\include 
